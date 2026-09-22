f702106b092edf9a5cd38cbcd0d026a49f0f9d8d
0306241407-star <0306241407@caothang.edu.vn>
2026-09-22 22:00:08 +0700

    xong script 3

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,46 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import NoteList from '../components/NoteListView';
+import React from 'react';
+import Notes from './Notes';
 
-type notes = {
-    
-        id: string,
-        title: string,
-        content: string,
-        createdAt: string,
-        updatedAt: string
-    
-}
 export const Home: React.FC = () => {
-  const [notes, setNotes] = useState<notes[] | []>([]);
-  useEffect(()=>{
-
-    (async()=>{
-      try {
-        const req = await fetch("http://localhost:5000/api/notes/cong-viec", {
-          method: "GET",
-          headers: {'Content-Type': 'application/json'}
-        });
-        const res = await req.json();
-        setNotes(res);
-      } catch (err) {
-        setNotes([]);
-      }
-    })()
-  }, []);
   return (
     <div>
-    <div className="flex justify-between">
-      <div>
-        <h2>Trang chủ (Dashboard / Public Notes)</h2>
-        <p>Nơi hiển thị các ghi chú theo chủ đề (Học tập, Công việc...).</p>
-      </div>
-      <div>
-        <button className='btn bg-blue-400'>New</button>
-      </div>
+      <Notes />
     </div>
-      {notes.length > 0 && <NoteList topic='cong-viec' notes={notes}></NoteList>}
-    </div>
-    
   );
 };
 
